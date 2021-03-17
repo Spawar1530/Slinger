fc15ac6f885a3e384dd0669ad14e9dcf429c5b69
Suraj Pawar <spawar1530@gmail.com>
2021-03-17 13:12:10 +0530

    updated version

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
+import Signup from "./components/Signup";
 import './App.css';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 const projectID = 'dd6c218e-6ab3-4955-8c41-0355387aff46';
 
-const App = () => {
+const App = (props) => {
 
-if (!localStorage.getItem('username')) return <LoginForm />;
+// if (!localStorage.getItem('username')) return <LoginForm />;
+if (!localStorage.getItem('username' && 'password')) return(
+  <BrowserRouter>
+  <Switch>
+  <Route exact path="/" component={ LoginForm }/>
+  <Route path="/signup" component={ Signup }/>
+  </Switch>
+  </BrowserRouter>
+) ;
 
-return (<ChatEngine
+return (<div><ChatEngine
   height="100vh"
   projectID={projectID}
   userName={localStorage.getItem('username')}
@@ -18,12 +28,16 @@ return (<ChatEngine
   renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
   onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
   />
-  // { <div>
-  // <Link to="/login">
-  // <button className="button">Log Out</button>
-  // <Link />
-  // </div> }
-
+  <div>
+  
+    <button className="signup__login1"
+    onClick={() => {
+      localStorage.removeItem('username' && "password");
+      <Redirect to='/' />
+      window.location.reload()
+    }}>Log Out</button>
+    </div>
+  </div>
 
 );
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,26 +1,27 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from "react-router-dom";
 
-const projectID = 'dd6c218e-6ab3-4955-8c41-0355387aff46';
+// const projectID = 'dd6c218e-6ab3-4955-8c41-0355387aff46';
+const projectKey = "ac35fbe3-9847-43f5-9a49-382c7306583e";
 
 const Modal = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
   const [error, setError] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    const authObject = { 'Project-ID': projectID, 'User-Name': username, 'User-Secret': password };
+    alert(`Account created successfully, sign in to get started`)
 
     try {
-      await axios.get('https://api.chatengine.io/chats', { headers: authObject });
-
-      localStorage.setItem('username', username);
-      localStorage.setItem('password', password);
-
-      window.location.reload();
+        axios.post(
+            'https://api.chatengine.io/projects/people/',
+            { 'username': username, 'secret': password, 'first_name': firstname, 'last_name': lastname },
+            { headers: { "Private-Key": projectKey } }
+        )
       setError('');
     } catch (err) {
       setError('Oops, incorrect credentials.');
@@ -30,19 +31,21 @@ const Modal = () => {
   return (
     <div className="wrapper">
       <div className="form">
-        <h1 className="title">Sign In</h1>
+        <h1 className="title">Sign Up</h1>
         <form onSubmit={handleSubmit}>
+        <input type="text" value={firstname} onChange={(e) => setFirstname(e.target.value)} className="input" placeholder="First name" required />
+          <input type="text" value={lastname} onChange={(e) => setLastname(e.target.value)} className="input" placeholder="Last name" required />
           <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
           <div align="center">
             <button type="submit" className="button">
-              <span>Let's Start</span>
+              <span>Submit</span>
             </button>
           </div>
           <p className="forgot-password text-left" style={{marginTop:"10%", marginLeft:"5%"}}>
-            Need an Account?{" "}
-              <Link className="nav-link" to={"/signup"}>
-            <span className="signup__login"> sign up</span>
+            Already have an Acconut?{" "}
+              <Link className="nav-link" to={"/"}>
+            <span className="signup__login">sign in</span>
         </Link>
       </p>
         </form>
@@ -54,3 +57,4 @@ const Modal = () => {
 };
 
 export default Modal;
+
